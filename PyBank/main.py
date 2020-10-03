59097c13e33fadeada60804f042b0379d41bda8d
Casio04 <carlos.casio@outlook.com>
2020-10-02 21:11:54 -0500

    This is the upload of the two requested homeworks

---
+++ PyBank/main.py
@@ -0,0 +1,59 @@
+
+import csv
+import os
+
+budget_bank = os.path.join("Resources","budget_data.csv")
+total_months = 0
+total_amount = 0
+average_change = 0
+next_value = 0
+actual_value = 0
+change_sum = 0
+next_increase = 0
+next_decrease = 0
+max_increase = 0
+max_decrease = 0
+
+# Open csv file with reading permissions
+with open(budget_bank, "r") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    # Skip header
+    header = next(csvreader)
+
+    for row in csvreader:
+            # Get the current values and store them in different vaariables to accomplish the max, min and average change questions.
+            actual_increase = next_increase
+            actual_decrease = next_decrease
+            next_value = int(row[1])
+            if csvreader.line_num > 2:
+                next_increase = next_value - actual_value
+                next_decrease = next_value - actual_value
+                change_sum = change_sum + (next_value - actual_value)
+                if next_increase > max_increase:
+                    max_increase = next_increase
+                    month_increase = row[0]
+
+                if next_decrease < max_decrease:
+                    max_decrease = next_decrease
+                    month_decrease = row[0]
+
+            total_months = total_months + 1
+            total_amount = (total_amount + int(row[1]))
+            average_change = total_amount / total_months
+            actual_value = next_value
+            
+    average_change = round(change_sum / (total_months - 1),2)
+    result =f"""
+    Financial Analyisis
+    --------------------------
+    Total Months: {total_months}
+    Total: ${total_amount}
+    Average Change: ${average_change}
+    Greatest Increase in Profits: {month_increase} (${max_increase})
+    Greatest Decrease in Profits: {month_decrease} (${max_decrease})
+    """ 
+    print(result)
+
+output_file = os.path.join("Analysis","PyBank_results.txt")
+
+open(output_file, "w").write(result)
---
+++ PyPoll/main.py
@@ -0,0 +1,58 @@
+
+import csv
+import os
+
+election_file = os.path.join("Resources","Resources_election_data.csv")
+voters_count = 0
+khan = 0
+Correy = 0
+Li = 0
+Tooley = 0
+with open(election_file, "r") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    next(csvreader)
+    for row in csvreader:
+        voters_count = voters_count + 1
+        if row[2] == "Khan":
+            khan = khan + 1
+        elif row[2] == "Correy":
+            Correy = Correy + 1
+        elif row[2] == "Li":
+            Li = Li + 1
+        elif row[2] == "O'Tooley":
+            Tooley = Tooley + 1
+    
+    khan_percent = round((khan / voters_count)*100, 2)
+    correy_percent = round((Correy / voters_count)*100, 2)
+    li_percent = round((Li / voters_count)*100, 2)
+    tooley_percent = round((Tooley / voters_count)*100, 2)
+    winner = max(khan, Correy, Li, Tooley)
+    if winner == khan:
+        winner = "Khan"
+    elif winner == Correy:
+        winner = "Correy"
+    elif winner == Li:
+        winner = "Li"
+    elif winner == Tooley:
+        winner = "O'Tooley"
+
+
+result = f"""
+Election Results
+-----------------------------
+Total Votes: {voters_count}
+-----------------------------
+Khan: {khan_percent}% ({khan})
+Correy: {correy_percent}% ({Correy})
+Li: {li_percent}% ({Li})
+O'Tooley: {tooley_percent}% ({Tooley})
+-----------------------------
+Winner: {winner}
+-----------------------------"""
+
+print(result)
+
+output_file = os.path.join("Analysis","election_results.txt")
+
+open(output_file, "w").write(result)
+
